e767a774e82b4f3b99c81aabbc354223f120ac2b
Colin McCulloch <colinmucculloch@gmail.com>
2020-03-18 11:37:18 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Navbar"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { currentPage: "Home" }
+    this.updatePage = this.updatePage.bind(this)
+  }
+  updatePage(newPage) {
+    console.log("in the update page function", newPage)
+    this.setState({ currentPage: newPage })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar
+          newNumber={95}
+          updatePage={this.updatePage}
+        />
+
+        <header className="App-header">
+          <img src="./images/cake.jpg" className="img-fluid" alt="logo" />
+          <p>
+            {this.state.currentPage}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+
 }
 
 export default App;
